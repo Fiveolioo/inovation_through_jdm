762391efa3b1f4b7ed7b59dcf3f7e642a9352fea
Fiveolioo <96richardohc@gmail.com>
2020-12-13 19:19:57 -1000

    create favs

---
+++ backend/cars/db/migrate/003_create_favs.rb
@@ -0,0 +1,11 @@
+
+class CreateFavs < ActiveRecord::Migration[5.2]
+    def change
+      create_table :favorites do |t|
+        t.references :user, foreign_key: true
+        t.references :car, foreign_key: true
+  
+        t.timestamps
+    end
+  end
+end
