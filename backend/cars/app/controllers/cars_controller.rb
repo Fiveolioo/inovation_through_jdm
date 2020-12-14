42fec9ff8f2bea358ed23fa30bcf4d94db6a8ea3
Fiveolioo <96richardohc@gmail.com>
2020-12-13 20:29:02 -1000

    create cars controller

---
+++ backend/cars/app/controllers/cars_controller.rb
@@ -0,0 +1,15 @@
+class CarsController < ApplicationController
+    def index
+        gifts = Gift.all
+        render json: gifts, except: [:created_at, :updated_at]
+    end
+
+    def show
+        gift = Gift.find_by(id: params[:id])
+        if gift
+            render json: gifts, except: [:created_at, :updated_at]
+        else
+            render json: {message: "Car not found :("}
+        end
+    end
+end
